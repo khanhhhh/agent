3de1c67b4c1cb690a96e95ea2a4ef9ba27ed9525
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-06 00:02:09 +0800

    modified:   src/model/modelImpl.go

--- src/model/modelImpl.go
+++ src/model/modelImpl.go
@@ -3,8 +3,8 @@ package model
 // Iterate :
 func Iterate(policy Policy, currentState State, action Action, nextState State, reward Reward, discount float64) (nextPolicy Policy) {
 	// update policy
-	_, utility := maxChoose(policy.Action(nextState))
-	newQuality := reward + discount*utility
+	_, nextBestQuality := maxChoose(policy.Action(nextState))
+	newQuality := reward + discount*nextBestQuality
 	policy.Update(currentState, action, newQuality)
 
 	// return
